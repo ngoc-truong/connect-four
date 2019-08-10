d1962b7c8a643da8d4a5a40eef5c68ee29bda332
Ngoc Truong <ngoc.truong@posteo.de>
2019-08-10 17:58:10 +0200

    Create game rounds and #start method

--- lib/connect-four.rb
+++ lib/connect-four.rb
@@ -1,18 +1,18 @@
 class Player 
-    @@allowed_colors = ["yellow", "red"]
-    attr_accessor :color 
+    @@allowed_signs = ["x", "o"]
+    attr_accessor :sign 
 
-    def initialize(color)
-        if @@allowed_colors.include?(color)
-            @color = color 
+    def initialize(sign)
+        if @@allowed_signs.include?(sign)
+            @sign = sign 
         else 
-            @color = nil 
+            @sign = nil 
         end 
     end 
 
     def choose_column
-        allowed = (1..7).to_a
-        puts "Please choose a column between 1 and 7!"
+        allowed = (0..6).to_a
+        puts "Please choose a column between 0 and 6!"
         column = gets.chomp
 
         while !allowed.include?(column.to_i)
@@ -25,28 +25,28 @@ class Player
 end
 
 class Board 
-    attr_accessor :board
+    attr_accessor :field
 
     def initialize 
-        @board = create_board
+        @field = create
     end
 
-    def create_board 
-        board = []
+    def create 
+        field = []
 
         6.times do 
             row = []
             7.times do 
                 row << "" 
             end
-            board << row 
+            field << row 
         end
 
-        board
+        field
     end
 
-    def print_board 
-        @board.each do |row| 
+    def print_board
+        @field.each do |row| 
             row.each do |cell| 
                 if cell == "" 
                     print "[ #{cell}]"
@@ -56,15 +56,236 @@ class Board
             end
             print "\n"
         end
+        puts "---------------------"
+        puts " 0  1  2  3  4  5  6"
+        puts "---------------------"
     end
+
 end 
 
 class Game 
     attr_accessor :player_one, :player_two, :board
 
     def initialize 
-        @player_one = Player.new("yellow")
-        @player_two = Player.new("red")
+        @player_one = Player.new("x")
+        @player_two = Player.new("o")
         @board = Board.new
     end
+
+    def start 
+        puts "Welcome to Connect Four! The first player who has four stones in a row wins! Have fun!"
+
+        until win? || board_full?
+            one_round 
+        end
+
+        puts "Hat er horizontal gewonnen: " + horizontal_win?.to_s
+        puts "Hat er vertikal gewonnen: " + vertical_win?.to_s
+        puts "Hat er diagonal gewonnen: " + diagonal_win?.to_s
+    end
+
+    def one_round 
+        puts ""
+        @board.print_board 
+
+        puts ""
+        puts "It's your turn, Player One ('x')."
+        choice = @player_one.choose_column
+        update_board(@player_one, choice) 
+        @board.print_board 
+
+        if win?
+            puts "You win!"
+            return 
+        end 
+
+        puts ""
+        puts "Now, it's your turn, Player Two ('o')."
+        choice = @player_two.choose_column
+        update_board(@player_two, choice) 
+        @board.print_board 
+    end
+
+    def update_board(player, column)
+        row = 5
+
+        while @board.field[row][column] != "" || row < 0
+            row -= 1
+
+            if row == -1
+                full_error = "Sorry, this column is full. Choose another one."
+                puts full_error  
+                return full_error 
+            end
+        end 
+
+        @board.field[row][column] = player.sign 
+    end
+
+    def board_full?
+        @board.field.each do |row| 
+            if row.include?("")
+                return false 
+            end
+        end 
+        true 
+    end
+
+    def win? 
+        horizontal_win? || vertical_win? || diagonal_win?
+    end
+
+    def horizontal_win? 
+        counter = 0
+        current_symbol = @player_one.sign 
+
+        @board.field.each do |row| 
+            row.each do |cell| 
+                if cell == current_symbol 
+                    counter += 1 
+                    return true if counter == 4
+                else 
+                    counter = 0 
+                    current_symbol = @player_two.sign 
+                end 
+            end
+            # Reset counter when going into new row
+            counter = 0
+        end
+        false 
+    end
+
+    def vertical_win? 
+        counter = 0
+        current_symbol = @player_one.sign
+        other_symbol = @player_two.sign 
+        placeholder = ""  
+        row = 0
+        column = 0
+
+        while column != 7 
+            row = 0
+            while row != 6
+                if @board.field[row][column] == current_symbol 
+                    counter += 1 
+                    return true if counter == 4
+                else 
+                    counter = 1 
+                    placeholder = current_symbol
+                    current_symbol = other_symbol
+                    other_symbol = placeholder 
+                end 
+                row += 1
+            end 
+            counter = 0
+            column += 1
+        end
+        false   
+    end
+
+    def diagonal_win? 
+        diagonals = all_diagonals
+        counter = 0
+        current_symbol = @player_one.sign 
+        other_symbol = @player_two.sign 
+        placeholder = "" 
+
+        diagonals.each do |diagonal| 
+            diagonal.each do |cell| 
+                if @board.field[cell[0]][cell[1]] == current_symbol
+                    counter += 1 
+                    return true if counter == 4
+                else 
+                    counter = 1 
+                    placeholder = current_symbol
+                    current_symbol = other_symbol
+                    other_symbol = placeholder
+                end
+            end 
+            counter = 0
+        end
+        false
+    end
+
+    # Helper methods for getting all diagonal combinations
+    def all_diagonals 
+        all_diagonals_asc + all_diagonals_desc
+    end
+
+    def all_diagonals_desc
+        all_diagonals = []
+        diagonals = []
+        x = 0
+        y = 0
+
+        4.times do |i| 
+            while x < 6 && y < 7 
+                diagonals << [x, y]
+                x += 1
+                y += 1
+            end 
+            all_diagonals << diagonals
+            diagonals = []
+            x = 0
+            y = i + 1
+        end 
+
+        x = 1
+        y = 0 
+
+        2.times do |i| 
+            while x < 6 && y < 7
+                diagonals << [x, y]
+                x += 1
+                y += 1
+            end
+            all_diagonals << diagonals 
+            diagonals = [] 
+            x = i + 2
+            y = 0
+        end
+
+        all_diagonals 
+    end
+
+
+    def all_diagonals_asc
+        all_diagonals = []
+        diagonals = []
+        x = 0
+        y = 3
+
+        4.times do |i| 
+            while x < 6 && y > -1
+                diagonals << [x, y]
+                x += 1
+                y -= 1
+            end 
+            all_diagonals << diagonals
+            diagonals = []
+            x = 0
+            y = i + 4
+        end 
+
+        x = 1
+        y = 6
+
+        2.times do |i| 
+            while x < 6 && y > -1
+                diagonals << [x, y]
+                x += 1
+                y -= 1
+            end
+            all_diagonals << diagonals 
+            diagonals = [] 
+            x = i + 2
+            y = 6
+        end
+
+        all_diagonals 
+    end
 end
+
+# ToDo: Win-Conditions are not correct yet! REFACTOR!
+game = Game.new 
+game.start
--- spec/connect-four_spec.rb
+++ spec/connect-four_spec.rb
@@ -1,41 +1,39 @@
 require "./lib/connect-four"
 
 describe Player do 
-    before { @player = Player.new("#{color}") } 
+    before { @player = Player.new("#{sign}") } 
 
-    context "Player has yellow figures" do 
-        let(:color) { "yellow" }
+    context "Player has sign x" do 
+        let(:sign) { "x" }
 
-        it "has yellow figure" do 
-            expect(@player.color).to eq("yellow")
+        it "has x symbol" do 
+            expect(@player.sign).to eq("x")
         end
     end 
 
-    context "Initiating player with wrong color" do 
-        let(:color) { "green" }
+    context "Initiating player with wrong sign" do 
+        let(:sign) { "green" }
 
-        it "does not set the color to green but nil" do 
-            expect(@player.color).to eq(nil)
+        it "does not set the sign to green but nil" do 
+            expect(@player.sign).to eq(nil)
         end
     end 
 
     context "Player can choose a number." do 
-        let(:color) { "yellow" }
+        let(:sign) { "x" }
 
         it "chooses a correct number" do 
-            expect(@player.choose_column).to be_between(1, 7)
+            expect(@player.choose_column).to be_between(0, 6)
         end 
     end
-
 end 
-
 
 describe Board do 
     before { @board = Board.new }
 
     context "Creating the board" do 
         it "creates a board in a two dimensional array" do 
-            expect(@board.create_board).to eq( [    ["", "", "", "", "", "", ""],
+            expect(@board.create).to eq( [    ["", "", "", "", "", "", ""],
                                                     ["", "", "", "", "", "", ""], 
                                                     ["", "", "", "", "", "", ""],
                                                     ["", "", "", "", "", "", ""],
@@ -58,4 +56,118 @@ describe Game do
             expect(@game.board).to be_a Board
         end
     end 
+
+    context "Game mechanics" do 
+        it "changes board when player one chooses column 3 in an empty field" do 
+            @game.update_board(@game.player_one, 3)
+            expect(@game.board.field[5][3]).to eql(@game.player_one.sign)
+        end 
+
+        it "gives an error if all fields in a column are full" do 
+            6.times do |num| 
+                @game.board.field[num][3] = "x"
+            end
+
+            expect(@game.update_board(@game.player_one, 3)).to eql("Sorry, this column is full. Choose another one.")
+        end
+
+        it "indicates when a board is not full" do 
+            expect(@game.board_full?).to eq(false)
+        end
+
+        it "indicates when a board is full" do 
+            6.times do |row| 
+                7.times do |cell| 
+                    if cell % 2 == 0
+                        @game.board.field[row][cell] = @game.player_one.sign 
+                    else 
+                        @game.board.field[row][cell] = @game.player_two.sign 
+                    end 
+                end 
+            end
+
+            expect(@game.board_full?).to eq(true)
+        end
+
+        it "indicates that a player wins with 4 stones in a row" do 
+            4.times do |cell| 
+                @game.board.field[0][cell] = @game.player_one.sign 
+            end 
+
+            expect(@game.horizontal_win?).to eq(true)
+        end 
+
+        it "indicates that a player does not win with three stones" do 
+            3.times do |cell| 
+                @game.board.field[0][cell] = @game.player_one.sign 
+            end 
+
+            expect(@game.horizontal_win?).to eq(false)
+        end 
+
+        it "indicates that player does not win when the fourth stone is in a new row" do 
+            3.times do |cell| 
+                @game.board.field[1][cell] = @game.player_one.sign 
+            end 
+
+            @game.board.field[0][6] = @game.player_one.sign 
+
+            expect(@game.horizontal_win?).to eq(false)
+        end
+
+
+        it "indicates when a player wins vertically" do 
+            4.times do |cell| 
+                @game.board.field[cell][3] = @game.player_one.sign 
+            end 
+
+            expect(@game.vertical_win?).to eq(true)
+        end 
+
+        it "indicates that player does not win when sum of old and new column is 4" do 
+            # Last cell of column 2
+            @game.board.field[5][2] = @game.player_one.sign 
+
+            # First three cells of column 3
+            3.times do |cell| 
+                @game.board.field[cell][3] = @game.player_one.sign 
+            end 
+
+            expect(@game.vertical_win?).to eq(false)
+        end 
+
+        it "indicates when a player does not win vertically" do 
+            3.times do |cell| 
+                @game.board.field[cell][3] = @game.player_one.sign 
+            end 
+
+            expect(@game.vertical_win?).to eq(false)
+        end 
+
+        it "indicates when a player wins diagonally descending" do
+            row = 1 
+            column = 0 
+           
+            4.times do 
+                @game.board.field[row][column] = @game.player_one.sign 
+                row += 1
+                column += 1
+            end
+
+            expect(@game.diagonal_win?).to eq(true)
+        end
+
+        it "indicates when a player wins diagonally ascending" do
+            row = 0
+            column = 6 
+           
+            4.times do 
+                @game.board.field[row][column] = @game.player_one.sign 
+                row += 1
+                column -= 1
+            end
+
+            expect(@game.diagonal_win?).to eq(true)
+        end
+    end
 end 
